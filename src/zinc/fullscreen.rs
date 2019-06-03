be84d93c90baa9160d1b13c25c79417fb5f28b5e
razvanoita <crioro@gmail.com>
2019-06-02 23:56:47 -0400

    +code to create vertex buffer for fullscreen pass

--- src/zinc/fullscreen.rs
+++ src/zinc/fullscreen.rs
@@ -8,5 +8,30 @@ vulkano::impl_vertex!(Vertex, position);
 
 pub struct FullscreenPass {
     gfx_queue: Arc<vulkano::device::Queue>,
-    vertex_buffer: Arc<vulkano::buffer::CpuAccessibleBuffer<[Vertex]>>
+    vertex_buffer: Arc<vulkano::buffer::CpuAccessibleBuffer<[Vertex]>>,
+    pipeline: Arc<vulkano::pipeline::GraphicsPipelineAbstract + Send + Sync>
+}
+
+impl FullscreenPass {
+    pub fn new<R>(gfx_queue: Arc<Queue>, subpass: Subpass<R>) -> FullscreenPass where R: vulkano::framebuffer::RenderPassAbstract + Send + Sync + 'static {
+        let vertex_buffer = vulkano::buffer::CpuAccessibleBuffer::from_iter(
+            gfx_queue.device().clone(), 
+            vulkano::buffer::BufferUsage::all(),
+            [
+                Vertex { 
+                    position: [-1.0, -1.0]
+                },
+                Vertex { 
+                    position: [-1.0, 3.0]
+                },
+                Vertex {
+                    position: [3.0, -1.0] 
+                }
+            ].iter().cloned()
+        ).expect("Failed to create vertex buffer for fullscreen pass!");
+
+        /*let pipeline = {
+            let vs = 
+        }*/
+    }
 }
