9d2f888ca318cad47cc2f2b5ba2fb5414264cc04
razvanoita <crioro@gmail.com>
2019-11-25 08:56:19 -0500

    -moar components

--- src/duralumin/mod.rs
+++ src/duralumin/mod.rs
@@ -7,8 +7,9 @@ use crate::aluminium::components;
 type Entity = usize;
 
 pub struct World {
-    pending_mask: Option<u32>
-    
+    pending_mask: Option<u32>,
+    pending_components: Vec<components::Component>,
+
     transform_components: HashMap<u32, components::TransformStorage>,
     mesh_components: HashMap<u32, components::MeshStorage>,
 }
@@ -18,40 +19,61 @@ impl World {
         let free_entities = vec![0];
         World {
             pending_mask: None,
-            transform_components: vec![],
-            mesh_components: vec![]
+            pending_components: vec![],
+            transform_components: HashMap::new(),
+            mesh_components: HashMap::new()
         }
     }
 
     pub fn create_entity(&mut self) -> &mut World {
+        assert_eq!(self.pending_components.is_empty(), true);
+
         self.pending_mask = Some(0);
         self
     }
 
     pub fn with_component(&mut self, component: components::Component) -> &mut World {
         assert_eq!(self.pending_mask.is_some(), true);
-
-        match component {
+       
+        match &component {
             components::Component::TransformComponent(transform_component) => {
-                self.transform_components.push(Some(components::TransformStorageEntry {
-                    value: transform_component,
-                    index: self.pending_entity.unwrap()
-                }))
+                self.pending_mask = Some(self.pending_mask.unwrap() | components::ComponentMask::TransformComponent as u32)
             },
             components::Component::MeshComponent(mesh_component) => {
-                self.mesh_components.push(Some(components::MeshStorageEntry {
-                    value: mesh_component,
-                    index: self.pending_entity.unwrap()
-                }))
-            }
-            _ => println!("not supported yet!")
+                self.pending_mask = Some(self.pending_mask.unwrap() | components::ComponentMask::MeshComponent as u32)
+            },
+            _ => println!("Component not supported!")
         }
+        self.pending_components.push(component);
 
         self
     }
 
-    pub fn build(&mut self) -> Entity {
-        self.pending_entity = None;
-        self.entities[self.entities.len() - 1]
+    pub fn build(&mut self) {
+        assert_eq!(self.pending_mask.is_some(), true);
+        assert_eq!(self.pending_components.is_empty(), false);
+
+        while !self.pending_components.is_empty() {
+            let component = self.pending_components.pop().unwrap();
+            match component {
+                components::Component::TransformComponent(transform_component) => {
+                    let mut value = self.transform_components.get_mut(&self.pending_mask.unwrap());
+                    if value.is_some() {
+                        value.unwrap().push(Some(transform_component));
+                    } else {
+                        self.transform_components.insert(self.pending_mask.unwrap(), vec![Some(transform_component)]);
+                    }
+                },
+                components::Component::MeshComponent(mesh_component) => {
+                    let mut value = self.mesh_components.get_mut(&self.pending_mask.unwrap());
+                    if value.is_some() {
+                        value.unwrap().push(Some(mesh_component));
+                    } else {
+                        self.mesh_components.insert(self.pending_mask.unwrap(), vec![Some(mesh_component)]);
+                    }
+                },
+                _ => println!("not supported yet!")
+            }
+        }
     }
 }
